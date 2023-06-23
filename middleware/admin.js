d301e4a5a8f7178db2fbff47dd25974618c71822
moegreene5 <agbasiifeanyi@gmail.com>
2023-06-23 09:30:38 +0100

    fist commit

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,4 +1,7 @@
+const config = require("config");
+
 function admin(req, res, next) {
+  if (!config.get("requiresAuth")) return next();
   if (!req.user.isAdmin) return res.status(403).send("Access Denied.");
   next();
 }
